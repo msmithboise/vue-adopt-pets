1368cf97100947c840a17c364edb1b9d633a5648
Michael <msmithboise@gmail.com>
2021-08-29 17:58:07 -0600

    setting up links

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>Cats for Adoption</h1>
-    <b-table striped hover :items="cats"></b-table>
+    <b-table striped hover :items="cats">
+      <template #cell(name)="data">
+        <!-- `data.value` is the value after formatted by the Formatter -->
+        <router-link :to="`/pets/${data.index}`">{{ data.value }}</router-link>
+      </template>
+    </b-table>
   </div>
 </template>
 
