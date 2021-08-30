1ee53a732a1a47c8ca69460a51fa0c302f9183c6
Michael <msmithboise@gmail.com>
2021-08-29 18:54:24 -0600

    pet ui changes

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    {{ this.$route.params.species }}
-    {{ this.$route.params.id }}
+    <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+    <p>Age: {{ animal.age }}</p>
+    <p>Breed: {{ animal.breed }}</p>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return { animal: {} };
+  },
+  computed: {
+    ...mapState(["cats", "dogs"]),
+  },
+  mounted() {
+    const animal = this[this.$route.params.species][this.$route.params.id];
+    this.animal = animal;
+  },
+};
+</script>
