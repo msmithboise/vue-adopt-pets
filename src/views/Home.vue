35ae1ffc743c4906cb373a0594b80eddd5676d08
Michael <msmithboise@gmail.com>
2021-08-29 19:09:33 -0600

    added form to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,65 @@
 <template>
   <div class="home">
     <h1>Adopt a new best friend.</h1>
+    <button @click="togglePetForm" class="btn btn-primary">Add New Pet</button>
+
+    <b-form @submit="handleSubmit" v-if="showPetForm">
+      <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="formData.name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Species:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="formData.species"
+          :options="['cats', 'dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Pet's Age:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="number"
+          v-model="formData.age"
+          placeholder="Enter age"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
   name: "home",
+  data() {
+    return {
+      showPetForm: false,
+      formData: {
+        name: "",
+        age: 0,
+        species: null,
+      },
+    };
+  },
+  methods: {
+    togglePetForm() {
+      this.showPetForm = !this.showPetForm;
+    },
+    handleSubmit() {},
+  },
 };
 </script>
 
